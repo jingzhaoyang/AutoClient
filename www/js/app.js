8d82f9db720f8ef61a1fde3232c86178f332303e
jingzhaoyang <jingzhaoyang@163.com>
2016-05-05 10:22:27 +0800

    init

---
+++ www/js/app.js
@@ -0,0 +1,16 @@
+angular.module('starter', ['ionic','starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+
---
+++ www/js/controllers.js
@@ -0,0 +1,199 @@
+angular.module('starter.controllers', [])
+
+.controller( 'forwardCtl',['$scope','$http',function($scope,$http){
+    // function go($scope,k){
+    //         $http.post('http://192.168.31.11/',{k:k},function(){});
+    // };
+    function go($scope,message) {
+        $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+        $http({
+            url: 'http://192.168.31.11/',
+            method: "post",
+            data: {"k":message}
+        })
+        .then(function(response) {
+          console.log("ok")
+                // success
+        },
+        function(response) { // optional
+          console.log("error")
+                // failed
+        });
+    };
+    $scope.onTouch=function(){
+      // alert("你触发了on-touch")
+      console.log("touch");
+      go($scope,"w");
+      console.log("w");
+    };
+    $scope.onRelease=function(){
+      console.log("relese");
+      go($scope,"stop");
+      console.log("stop");
+    };
+}])
+
+.controller( 'reverseCtl',['$scope','$http',function($scope,$http){
+    // function go($scope,k){
+    //         $http.post('http://192.168.31.11/',{k:k},function(){});
+    // };
+    function go($scope,message) {
+        $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+        $http({
+            url: 'http://192.168.31.11/',
+            method: "post",
+            data: {"k":message}
+        })
+        .then(function(response) {
+          console.log("ok")
+                // success
+        },
+        function(response) { // optional
+          console.log("error")
+                // failed
+        });
+    };
+    $scope.onTouch=function(){
+      // alert("你触发了on-touch")
+      console.log("touch");
+      go($scope,"s");
+      console.log("s");
+    };
+    $scope.onRelease=function(){
+      console.log("relese");
+      go($scope,"stop");
+      console.log("stop");
+    };
+}])
+
+.controller( 'leftCtl',['$scope','$http',function($scope,$http){
+    // function go($scope,k){
+    //         $http.post('http://192.168.31.11/',{k:k},function(){});
+    // };
+    function go($scope,message) {
+        $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+        $http({
+            url: 'http://192.168.31.11/',
+            method: "post",
+            data: {"k":message}
+        })
+        .then(function(response) {
+          console.log("ok")
+                // success
+        },
+        function(response) { // optional
+          console.log("error")
+                // failed
+        });
+    };
+    $scope.onTouch=function(){
+      // alert("你触发了on-touch")
+      console.log("touch");
+      go($scope,"a");
+      console.log("a");
+    };
+    $scope.onRelease=function(){
+      console.log("relese");
+      go($scope,"stop");
+      console.log("stop");
+    };
+}])
+
+.controller( 'rightCtl',['$scope','$http',function($scope,$http){
+    // function go($scope,k){
+    //         $http.post('http://192.168.31.11/',{k:k},function(){});
+    // };
+    function go($scope,message) {
+        $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+        $http({
+            url: 'http://192.168.31.11/',
+            method: "post",
+            data: {"k":message}
+        })
+        .then(function(response) {
+          console.log("ok")
+                // success
+        },
+        function(response) { // optional
+          console.log("error")
+                // failed
+        });
+    };
+    $scope.onTouch=function(){
+      // alert("你触发了on-touch")
+      console.log("touch");
+      go($scope,"d");
+      console.log("d");
+    };
+    $scope.onRelease=function(){
+      console.log("relese");
+      go($scope,"stop");
+      console.log("stop");
+    };
+}])
+
+.controller( 'stopCtl',['$scope','$http',function($scope,$http){
+    // function go($scope,k){
+    //         $http.post('http://192.168.31.11/',{k:k},function(){});
+    // };
+    function go($scope,message) {
+        $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+        $http({
+            url: 'http://192.168.31.11/',
+            method: "post",
+            data: {"k":message}
+        })
+        .then(function(response) {
+          console.log("ok")
+                // success
+        },
+        function(response) { // optional
+          console.log("error")
+                // failed
+        });
+    };
+    $scope.onTouch=function(){
+      console.log("relese");
+      go($scope,"stop");
+      console.log("stop");
+    };
+}])
+
+.controller( 'toggleAuto',['$scope','$http',function($scope,$http){
+    // function go($scope,k){
+    //         $http.post('http://192.168.31.11/',{k:k},function(){});
+    // };
+    function go($scope,message) {
+        $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+        $http({
+            url: 'http://192.168.31.11/',
+            method: "post",
+            data: {"k":message}
+        })
+        .then(function(response) {
+          console.log("ok")
+                // success
+        },
+        function(response) { // optional
+          console.log("error")
+                // failed
+        });
+    };
+    $scope.value = false;
+    console.log($scope.value);
+    $scope.toggleChange = function() {
+      console.log($scope.value);
+      if ($scope.value == true){
+          console.log("start");
+          $scope.value = true;
+          console.log("start");
+          go($scope,"auto");
+          console.log("Auto");
+        } else {
+          $scope.value = false;
+          console.log("Autosop");
+          go($scope, "stop");
+          console.log("Stop");
+        };
+    };
+}])
